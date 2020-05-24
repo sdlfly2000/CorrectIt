89b5b8d7b30b125017dedfdabd01d8926c33521e
Jay Shi <sdlfly2000@163.com>
2020-05-24 22:36:27 +0800

    Fix compile issue

---
+++ Domain.Service/QuestionImages/Gateways/Persistors/Contracts/CreateQuestionImageRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Domain.Services.QuestionImages.Gateways.Persistors.Contracts
+{
+    public class CreateQuestionImageRequest
+    {
+
+        public string ImageCategory { get; set; }
+        public string ImageFileName { get; set; }
+        public string ImageCreatedBy { get; set; }
+        public string ImageComments { get; set; }
+
+        public Guid QuestionId { get; set; }
+    }
+}
