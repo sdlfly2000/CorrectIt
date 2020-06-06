2140498c1ac9ebeef75040b6dbe6f9eadf53e69e
Jay Shi <sdlfly2000@163.com>
2020-06-07 04:02:10 +0800

    https://gitee.com/sdlfly2000/CorrectIt/issues/I1J4PR

--- Infrastructure.Data.File/ImageFiles/ImageFilePersistor.cs
+++ Infrastructure.Data.File/ImageFiles/ImageFilePersistor.cs
@@ -1,6 +1,7 @@
 ﻿using Common.Core.DependencyInjection;
 using System;
 using OpenCvSharp;
+using Microsoft.Extensions.Configuration;
 using System.Runtime.InteropServices;
 
 namespace Infrastructure.Data.File.ImageFiles
@@ -8,10 +9,19 @@ namespace Infrastructure.Data.File.ImageFiles
     [ServiceLocate(typeof(IImageFilePersistor))]
     public class ImageFilePersistor : IImageFilePersistor
     {
+        private readonly IConfiguration _configuration;
+        private string keyImageFileFolder = "FeatureFlags:ImageFileFolder";
+
+        public ImageFilePersistor(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public Guid SaveToDisk(byte[] imageData, int imageWidth, int imageHeight)
         {
             var guidName = Guid.NewGuid();
-            var fileName = guidName + ".jpeg";
+            var folder = GetFolderName();
+            var fileName = folder + guidName + ".jpeg";
             var imgPtr = Marshal.AllocHGlobal(imageWidth * imageHeight * 3);
             try
             {
@@ -27,5 +37,14 @@ namespace Infrastructure.Data.File.ImageFiles
                 Marshal.FreeHGlobal(imgPtr);
             }
         }
+
+        #region Private Methods
+
+        private string GetFolderName()
+        {
+            return _configuration.GetValue<string>(keyImageFileFolder);
+        }
+
+        #endregion
     }
 }
