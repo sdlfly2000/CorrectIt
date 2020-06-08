9c2cf090b4c6dfd96e43463ac9b46bba24564691
Jay Shi <JayShi@eurofins.com>
2020-06-08 22:02:10 +0800

    UI Layout

--- Presentation.Mvc/ClientApp/src/app/app.module.ts
+++ Presentation.Mvc/ClientApp/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './mainPanel/Questions/questions.component';
 import { TitleBarComponent } from './titleBar/titlebar.component';
+import { NavBarComponent } from './navBar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuestionComponent,
-    TitleBarComponent
+    TitleBarComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
---
+++ Presentation.Mvc/ClientApp/src/app/navBar/navbar.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navBar',
+  templateUrl: './navbar.component.html'
+})
+export class NavBarComponent { }
