3a6f95ab720f4f1eb67c4594338a894823968483
Jay Shi <sdlfly2000@163.com>
2020-06-17 22:52:07 +0800

    https://gitee.com/sdlfly2000/CorrectIt/issues/I1JST1

--- Presentation.Mvc/ClientApp/src/app/app-routing.module.ts
+++ Presentation.Mvc/ClientApp/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { QuestionComponent } from './mainPanel/Questions/questions.component';
+
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+  //{ path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  //{ path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+  { path: '', component: QuestionComponent}
 ];
 
 @NgModule({
