1d3ba7d4f22895bdb8bd539e21da364900b7055b
Jay Shi <JayShi@eurofins.com>
2020-08-14 17:29:40 +0800

    Initial Redis

---
+++ Common.Core.Cache/AspectCacheAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Extensions.Caching.Redis;
+using Microsoft.Extensions.Configuration;
+
+namespace Common.Core.Cache
+{
+    using StackExchange.Redis;
+
+    public class AspectCacheAttribute : Attribute
+    {
+        private static RedisCacheOptions _redisCacheOptions;
+        private static RedisCache _redisCache;
+        private IConfiguration _configuration;
+
+        public AspectCacheAttribute(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            _redisCacheOptions = new RedisCacheOptions
+             {
+                ConfigurationOptions = new ConfigurationOptions
+               {
+
+               }
+             };
+            _redisCache = new RedisCache(_redisCacheOptions);
+        }
+
+        public static void Cache<T>() where T : ICache
+        {
+
+        }
+    }
+}
---
+++ Common.Core.Cache/ICache.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Core.Cache
+{
+    public class ICache
+    {
+    }
+}
